658d22411f993a92f3d6df42b416cf92516a35b1
Steven Shepherd <srshepherdjr@gmail.com>
2025-11-06 11:29:28 -0500

    Initial Commit

---
+++ Maintenance Logger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Maintenance Logger
+//
+//  Created by Steven Shepherd on 11/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Maintenance Logger/Maintenance_LoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Maintenance_LoggerApp.swift
+//  Maintenance Logger
+//
+//  Created by Steven Shepherd on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Maintenance_LoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
