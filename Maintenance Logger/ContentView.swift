c99578db5bd4d18c171d6b69d829797b762221e7
Steven Shepherd <srshepherdjr@gmail.com>
2025-11-07 14:30:32 -0500

    Initial project code

--- Maintenance Logger/ContentView.swift
+++ Maintenance Logger/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
     var body: some View {
         VStack {
+
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .padding(10)
+            Image(systemName: "arrow.up")
+                .cornerRadius(20)
+                .padding(10)
+                .onTapGesture {apGesture in
+                    
+                }
+            Text("Hello, new world!")
+                .padding(5)
+            Button("Quit") {
+                exit(0)
+            }
+            .padding(5)
+            Button("Show Alert") {
+                showingAlert = true
+            }
+            .alert("Important Message", isPresented: $showingAlert) {
+                Button("OK") {
+                    // Handle OK button tap
+                    print("OK tapped")
+                }
+                Button("Cancel", role: .cancel) {
+                    // Handle Cancel button tap
+                    print("Cancel tapped")
+                }
+            } message: {
+                Text("Please read this message carefully.")
+            }
+
         }
-        .padding()
+      
     }
 }
 
